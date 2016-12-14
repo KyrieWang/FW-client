767bf55fea7e12ec447b9393969e027de36f5471
BinWang <747910316@qq.com>
2016-12-14 20:13:14 +0800

    添加功能：能够返回配置规则成功的确认信息

--- FWclient/FWclient/tools/SendInfo.cs
+++ FWclient/FWclient/tools/SendInfo.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using SharpPcap;
+using PacketDotNet;
 
 namespace FirewallClientTest
 {
@@ -15,7 +17,8 @@ namespace FirewallClientTest
     /// </summary>
     class SendInfo
     {
-        DeviceForm devform;
+        private  DeviceForm devform;
+        private static bool config_info_confirm;
 
         /*初始化设备*/
         public SendInfo(DeviceForm devform)
@@ -27,8 +30,10 @@ namespace FirewallClientTest
         /// 发送配置信息
         /// </summary>
         /// <param name="cmd">需要配置的规则</param>
-        public void SendConfigInfo(string cmd)
+        public bool  SendConfigInfo(string cmd)
         {
+            config_info_confirm = false;
+
             byte[] head = { 0x0f, 0x0f };   //自定义数据包包头
             byte[] body = Encoding.ASCII.GetBytes(cmd + "!");
             byte[] data = head.Concat(body).ToArray();
@@ -38,18 +43,33 @@ namespace FirewallClientTest
             int remotePort = devform.getDev_port();
             IPEndPoint remotePoint = new IPEndPoint(remoteIP, remotePort);
 
-            client = new UdpClient();
+            CaptureDeviceList devices = CaptureDeviceList.Instance;
+            // If no devices were found print an error
+            if (devices.Count < 1)
+            {
+                // Console.WriteLine("No devices were found on this machine");
+                return false;
+            }
+            ICaptureDevice device = devices[0];
+            device.OnPacketArrival += new PacketArrivalEventHandler(configDev_OnPacketArrival);
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+            string filter = "ip and udp";
+            device.Filter = filter;
+            device.StartCapture();
 
 #if debug
             Console.WriteLine("start sending:");
 #endif
-
+            client = new UdpClient();
             client.Send(data, data.Length, remotePoint);
             client.Close();
-
 #if debug
             Console.WriteLine("send successfully!");
 #endif
+            device.StopCapture();
+            device.Close();
+            return config_info_confirm;
         }
 
         /// <summary>
@@ -70,18 +90,42 @@ namespace FirewallClientTest
             IPEndPoint remotePoint = new IPEndPoint(remoteIP, remotePort);
 
             client = new UdpClient(); 
-
 #if debug
             Console.WriteLine("start sending:");
 #endif
-
             client.Send(data, data.Length, remotePoint);
             client.Close();
-
 #if debug
             Console.WriteLine("send successfully!");
-#endif
-            
+#endif     
+        }
+
+        private static void configDev_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            //var time = e.Packet.Timeval.Date;
+            //var len = e.Packet.Data.Length;
+
+            var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+            UdpPacket udpPacket = UdpPacket.GetEncapsulated(packet);
+
+            if (udpPacket != null)
+            {
+                var ipPacket = (PacketDotNet.IpPacket)udpPacket.ParentPacket;
+                System.Net.IPAddress srcIp = ipPacket.SourceAddress;
+                int srcPort = udpPacket.SourcePort;
+                int dstPort = udpPacket.DestinationPort;
+
+                if (srcPort == 30330 && dstPort == 30331)
+                {
+                    byte[] payload = udpPacket.PayloadData;
+                    string content = System.Text.Encoding.Default.GetString(payload); //确认包的内容
+                    if (content == "yes")
+                    {
+                        config_info_confirm = true;
+                    }
+                }
+            }
         }
     }
 }

4aac989239dcc9052dd69bafd9938810d6bb6b22
BinWang <747910316@qq.com>
2016-12-14 20:10:55 +0800

    变更DeviceForm功能，作为所有设备类型的基类

--- FWclient/FWclient/forms/DeviceForm.cs
+++ FWclient/FWclient/forms/DeviceForm.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace FirewallClientTest
 {
     /// <summary>
-    /// 受保护设备
+    /// 一般设备，可代表受保护设备
     /// </summary>
     class DeviceForm
     {
         private string dev_IP;  //设备ip
         private int dev_port;   //设备端口
 
-        public void setDev_IPAndDev_port(string dev_IP, int dev_port)
+        public DeviceForm (string dev_IP, int dev_port)
         {
             this.dev_IP = dev_IP;
             this.dev_port = dev_port;
@@ -28,6 +28,16 @@ namespace FirewallClientTest
         public int getDev_port()
         {
             return dev_port;
+        }
+
+        public void setDev_IP(string dev_IP)
+        {
+            this.dev_IP = dev_IP;
+        }
+
+        public void setDev_port(int dev_port)
+        {
+            this.dev_port = dev_port;
         }
     }
 }
